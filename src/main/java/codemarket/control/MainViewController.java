916f8702f53a20450a38710504cb622039d93e89
luishresende <luishresende@outlook.com.br>
2023-09-08 22:43:02 -0300

    Tela  PDV - Ajuste e Implementação
    Ajustado a responsividade da tela, perimindo o seu redimensionameto, e funcionamento com o menu aberto. Implementada no programa principal.

--- src/main/java/codemarket/control/MainViewController.java
+++ src/main/java/codemarket/control/MainViewController.java
@@ -164,6 +164,7 @@ public class MainViewController implements Initializable {
 
             // Definindo o conteúdo do AnchorPane existente como o novo conteúdo carregado
             applicationAnchorPane.getChildren().setAll(novoConteudo);
+            System.out.println(applicationAnchorPane.getHeight());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -204,6 +205,28 @@ public class MainViewController implements Initializable {
 
             // Definindo o conteúdo do AnchorPane existente como o novo conteúdo carregado
             applicationAnchorPane.getChildren().setAll(novoConteudo);
+            System.out.println(applicationAnchorPane.getHeight());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+        public void handlePDVButton() {
+        applicationAnchorPane.getChildren().clear(); // Limpando o conteudo do AnchorPane pai
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/PDVView.fxml"));
+        try {
+            // Carregando o conteudo da nova tela
+            AnchorPane novoConteudo = loader.load();
+
+            // Definindo as costraints do novo conteudo para ocupar 100% da tela
+            AnchorPane.setBottomAnchor(novoConteudo, 0.0);
+            AnchorPane.setTopAnchor(novoConteudo, 0.0);
+            AnchorPane.setRightAnchor(novoConteudo, 0.0);
+            AnchorPane.setLeftAnchor(novoConteudo, 0.0);
+
+            // Definindo o conteúdo do AnchorPane existente como o novo conteúdo carregado
+            applicationAnchorPane.getChildren().setAll(novoConteudo);
+            System.out.println(applicationAnchorPane.getHeight());
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/codemarket/control/PDVViewController.java
@@ -0,0 +1,13 @@
+package codemarket.control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class PDVViewController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
