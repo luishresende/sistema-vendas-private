d7fde0142e1727aaf5094d228de997444281c408
Luis Henrique da Silva Resende <luishresende@outlook.com.br>
2023-11-07 08:44:59 -0300

    Atualização da tela de configuração do banco de dados
    Atualizado a tela da configuração do banco de dados com base no novo modelo de criptografia

--- src/main/java/codemarket/control/DialogDatabaseSettingsViewController.java
+++ src/main/java/codemarket/control/DialogDatabaseSettingsViewController.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package codemarket.control;
 
-import codemarket.model.conexao.ConexaoHibernate;
-import java.io.File;
+import codemarket.model.conexao.PersistenceManipulation;
 import java.net.URL;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -19,18 +12,8 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javax.persistence.EntityManager;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 
 public class DialogDatabaseSettingsViewController implements Initializable {
-
     @FXML
     private TextField textFieldEnderecoDB;
     @FXML
@@ -46,8 +29,9 @@ public class DialogDatabaseSettingsViewController implements Initializable {
 
     private Stage dialogStage;
     private boolean buttonSalvarClicked = false;
-    private File settingsFile = new File("./src/main/resources/META-INF/persistence.xml");
+    private PersistenceManipulation persistenceManipulation;
 
+    
     public Stage getDialogStage() {
         return dialogStage;
     }
@@ -64,33 +48,6 @@ public class DialogDatabaseSettingsViewController implements Initializable {
         this.buttonSalvarClicked = buttonSalvarClicked;
     }
 
-    public boolean validateNewSettings(String endereco, String dbNome, String usuario, String senha) {
-        if (!endereco.contains(":") || endereco.contains("/") || endereco.substring(endereco.indexOf(":")).isEmpty()) {
-            return false;
-        }
-
-        return !dbNome.isEmpty() && !usuario.isEmpty() && !senha.isEmpty();
-    }
-
-    public boolean isValidConnection() throws SQLException {
-        ConexaoHibernate conexao = new ConexaoHibernate();
-        EntityManager manager = conexao.getInstance();
-
-        boolean connected = false;
-
-        try {
-            // Tentar executar uma consulta simples para validar a conexão
-            manager.createQuery("SELECT 1");
-            connected = true;
-        } catch (Exception e) {
-            connected = false;
-        } finally {
-            manager.close();
-        }
-
-        return connected;
-    }
-
     private void displayErrorScreen() {
         Alert alert = new Alert(AlertType.ERROR);
         alert.setTitle("Erro");
@@ -102,142 +59,19 @@ public class DialogDatabaseSettingsViewController implements Initializable {
 
     @FXML
     public void handlerButtonSalvar() {
-        try {
-            // Definição de objetos responsáveis pir contruir os documentos XML
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-
-            // Crio os documentos passando o já existente como parametro
-            org.w3c.dom.Document doc = dBuilder.parse(settingsFile);
-            org.w3c.dom.Document oldDoc = dBuilder.parse(settingsFile);
-            doc.getDocumentElement().normalize();
-
-            // Obtendo todos os elementos com a tag property
-            NodeList properties = doc.getElementsByTagName("property");
-            Element urlElement = (Element) properties.item(0);
-            Element userElement = (Element) properties.item(1);
-            Element passwordElement = (Element) properties.item(3);
-            
-            // Definindo cada uma para as variaveis
-            String urlConfig = urlElement.getAttribute("value");
-            String usernameConfig = userElement.getAttribute("value");
-            String passwordConfig = passwordElement.getAttribute("value");
-
-            // Manipulação de strings para obter apenas o conteudo desejado de determinados atributos
-            int indexInicial = urlConfig.indexOf("//") + 2;
-            int indexFinal = urlConfig.indexOf("/", indexInicial);
-            String schema;
-            try {
-                int indexInterrogacao = urlConfig.indexOf("?", indexInicial);
-                schema = urlConfig.substring(indexFinal + 1, indexInterrogacao);
-            } catch (Exception e) {
-                schema = urlConfig.substring(indexFinal + 1);
-            }
-
-            // Definindo variáveis para receberem os valores dos campos de texto com as entradas do usuario
-            String endereco = textFieldEnderecoDB.getText();
-            String dbNome = textFieldNomeDB.getText();
-            String usuario = textFieldUsuarioDB.getText();
-            String senha = passwordFieldSenhaDB.getText();
-
-            // Validando os dados
-            if (validateNewSettings(endereco, dbNome, usuario, senha)) {
-                // Atribuindo as novas propriedades ao documento XML
-                properties.item(0).setTextContent(urlConfig.substring(0, indexInicial) + endereco + "/" + dbNome);
-                properties.item(1).setTextContent(usuario);
-                properties.item(3).setTextContent(senha);
-
-                // Definição do objeto transformador, responsável por editar e salvar o XML
-                TransformerFactory transformerFactory = TransformerFactory.newInstance();
-                javax.xml.transform.Transformer transformer = transformerFactory.newTransformer();
-                //transformer.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, "-//Hibernate/Hibernate Configuration DTD 3.0//EN");
-
-                // Definindo o source de ambos os documentos, antigo(atual) e novo
-                DOMSource source = new DOMSource(doc);
-                DOMSource oldSource = new DOMSource(oldDoc);
-
-                // Criando um novo arquivo que receberá o documento XML sem as alterações aplicadas pelo usuário
-                File oldFile = new File("./src/main/resources/META-INF/oldPersistence.xml");
-
-                // Atribuindo os antigos valores do XML para o "antigo" arquivo
-                StreamResult oldResult = new StreamResult(oldFile);
-                transformer.transform(oldSource, oldResult);
-
-                // Atribuindo os novos valores para o arquivo de configuração padrão do hibernate e atualizando a variável que armazena ele na classe
-                StreamResult result = new StreamResult(new File("./src/main/resources/META-INF/persistence.xml"));
-                transformer.transform(source, result);
-                settingsFile = new File("./src/main/resources/META-INF/persistence.xml");
-
-                // Validanddo conexão
-                if (isValidConnection()) {
-                    // Fecho a janela
-                    dialogStage.close();
-                } else {
-                    // Retorno os atribuitos antigos para não serem perdidos
-                    transformer.transform(oldSource, result);
-                    settingsFile = new File("./src/main/resources/META-INF/persistence.xml");
-                    displayErrorScreen();
-                }
-                // Apago o arquivo XML que possuia os antigos valores
-                oldFile.delete();
-            } else {
-
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void loadSettings() {
-        System.out.println(settingsFile.getAbsolutePath());
-        try {
-            // Definindo o objeto responsável por "ler" o XML
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            
-            // Normalizando o documento
-            org.w3c.dom.Document doc = dBuilder.parse(settingsFile);
-            doc.getDocumentElement().normalize();
-
-            // Obtendo todos os elementos com a tag property
-            NodeList nodeList = doc.getElementsByTagName("property");
-            Element urlElement = (Element) nodeList.item(0);
-            Element userElement = (Element) nodeList.item(1);
-            Element passwordElement = (Element) nodeList.item(3);
-            
-            // Definindo cada uma para as variaveis
-            String urlConfig = urlElement.getAttribute("value");
-            String usernameConfig = userElement.getAttribute("value");
-            String passwordConfig = passwordElement.getAttribute("value");
-
-            // Manipulação de strings para obtenção do conteudo desejado
-            int indexInicial = urlConfig.indexOf("//") + 2;
-            int indexFinal = urlConfig.indexOf("/", indexInicial);
-            System.out.println(indexInicial);
-            System.out.println(indexFinal);
-            String schema;
-            try {
-                int indexInterrogacao = urlConfig.indexOf("?", indexInicial);
-                schema = urlConfig.substring(indexFinal + 1, indexInterrogacao);
-            } catch (Exception e) {
-                schema = urlConfig.substring(indexFinal + 1);
-            }
-            String endereco = urlConfig.substring(indexInicial, indexFinal);
-
-            // Aplicação dos valores armazenado no XML nos fields, possibilitando a visualização dos mesmos.
-            textFieldEnderecoDB.setText(endereco);
-            textFieldNomeDB.setText(schema);
-            textFieldUsuarioDB.setText(usernameConfig);
-            passwordFieldSenhaDB.setText(passwordConfig);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        if(persistenceManipulation.updatePersistence(textFieldEnderecoDB.getText(), textFieldUsuarioDB.getText(), passwordFieldSenhaDB.getText(), textFieldNomeDB.getText())){
+            getDialogStage().close();
+        }else {
+            displayErrorScreen();
         }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        loadSettings();
+        try {
+            this.persistenceManipulation = new PersistenceManipulation();
+        } catch (Exception ex) {
+            System.out.println("nao funfou");
+        }
     }
 }
